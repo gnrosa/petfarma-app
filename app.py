38b708d4d13b958cb755bf8016e51107514d5eb9
Guilherme Nunes Rosa <guilherme.rosa@ciccr-sp.interno>
2019-03-29 11:23:02 -0300

    add funcionalities

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, session, abort, flash
 from flask_sqlalchemy import SQLAlchemy
-from forms import FormConsulta
+from forms import FormConsulta, LoginForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.urandom(12)
 db = SQLAlchemy(app)
 
 class Pedido(db.Model):
@@ -21,3 +22,24 @@ def index():
 	resultado = Pedido.query.filter_by(id=id).first()
 	return render_template('index.html', resultado=resultado, form=form)
 
+@app.route('/admin')
+def admin():
+	pedidos = Pedido.query.all()
+	form = LoginForm()
+	if not session.get('logged_in'):
+		return render_template('login.html', form=form)
+	else:
+		return render_template('admin.html', pedidos=pedidos)
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+	if request.form['password'] == 'password' and request.form['username'] == 'admin':
+		session['logged_in'] = True
+	else:
+		flash('Verifique o usuário e senha')
+	return admin()
+
+@app.route('/logout', methods=['POST'])
+def logout():
+	session['logged_in'] = False
+	return redirect('admin')
--- forms.py
+++ forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField
+from wtforms import IntegerField, PasswordField, StringField
 from wtforms.validators import DataRequired
 
 class FormConsulta(FlaskForm):
 	id = IntegerField('Número do pedido', validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+	username = StringField('Usuário', validators=[DataRequired()])
+	password = PasswordField('Senha', validators=[DataRequired()])
