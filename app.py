55de628f48143eff34791450c80a66e5cbf16010
Guilherme Nunes Rosa <guilherme.rosa@ciccr-sp.interno>
2019-03-29 12:50:58 -0300

    debug activate

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def logout():
 if __name__ == '__main__':
 	db.create_all()
 	port = int(os.environ.get("PORT",5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port, debug=True)
